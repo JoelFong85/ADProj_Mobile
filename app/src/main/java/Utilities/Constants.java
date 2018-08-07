1a57f9f13ff73890838263aca7280de460917289
Avergrove <e0284007@u.nus.edu>
2018-08-07 09:30:43 +0800

    Tharrani comment for activity and deleted extra classes

--- app/src/main/java/Utilities/Constants.java
+++ app/src/main/java/Utilities/Constants.java
@@ -3,7 +3,7 @@ package Utilities;
 public class Constants {
 
     // Session information
-    public static final String SERVICE_HOST = "http://172.17.147.64/LogicUniversity/Services/AndroidService.svc"; // Server to connect to.
+    public static final String SERVICE_HOST = "http://172.17.110.196/LogicUniversity/Services/AndroidService.svc"; // Server to connect to.
     public static String TOKEN = "7a654800-f3ca-4f9c-94c2-7b839929076c";
 
     // Roles
